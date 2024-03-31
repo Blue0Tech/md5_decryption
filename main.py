8c0764d49e4d4097e10495cc0f6a796ca8262a3e
Blue0Tech <pruthvishrikaanth@gmail.com>
2024-03-31 12:06:33 +0100

    Anagram solver with word list and hash

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def find_hash(original_hash):
     word_file = open("words.txt","r")
     word_file = list(word_file)
 
-    anagram = "who outlay thieves"
+    anagram = "i move lads"
     words = anagram.count(' ')
     words += 1
 
@@ -16,14 +16,24 @@ def find_hash(original_hash):
 
     final_words = []
 
-    #Student Activity
+    for word in word_file:
+        word = word.replace('\n','')
+        word_chars = list(set(word))
+        flag = False
+        for c in word_chars:
+            if(c not in char_list):
+                flag = True
+                break
+        if(not flag):
+            final_words.append(word)
 
     print(len(final_words))
 
     for elem in permutations(final_words, words):
         hash_elem = " ".join(elem)
         
-        #Student Activity
+        if(len(hash_elem) != len(anagram)):
+            continue
         
         m = hashlib.md5()
         m.update(hash_elem.encode('utf-8'))
@@ -32,6 +42,6 @@ def find_hash(original_hash):
         if word_hash == original_hash:
             return hash_elem
 
-hash = '13b382e1a2f8e22535b4730d78bc8591'
+hash = 'ac3751fa101668c6de2002356d9a032b'
 answer = find_hash(hash)
 print(f"Collision!  The word corresponding to the given hash is '{answer}'")
